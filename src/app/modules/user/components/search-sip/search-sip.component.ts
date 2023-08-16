ecb1a4b143783316464e596572c771381ed3cb3c
Luisa <hanna9653@gmail.com>
2023-08-15 19:22:45 -0500

    Ajustes estilos

--- src/app/modules/user/components/search-sip/search-sip.component.ts
+++ src/app/modules/user/components/search-sip/search-sip.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-search-sip',
   templateUrl: './search-sip.component.html',
-  styleUrls: ['./search-sip.component.scss']
+  styleUrls: ['./search-sip.component.scss'],
 })
 export class SearchSipComponent {
+  constructor(private router: Router) {}
 
+  public onSearch() {
+    this.router.navigate(['/user/see']);
+  }
 }
