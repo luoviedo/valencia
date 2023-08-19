4b6cc6d43952383633c16dbc6e7582c9ed926577
Luisa <hanna9653@gmail.com>
2023-08-18 19:53:28 -0500

    ajustes

---
+++ src/app/modules/user/components/social-history/social-history.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-social-history',
+  templateUrl: './social-history.component.html',
+  styleUrls: ['./social-history.component.scss']
+})
+export class SocialHistoryComponent {
+
+}
