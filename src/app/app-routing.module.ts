1b907ba935d5722845157af2f24ed4d888942490
Jose Mario Barrios <jmbarrios@indracompany.com>
2023-08-23 15:27:58 -0500

    cambios login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,6 +6,10 @@ const routes: Routes = [
         path: 'user',
         loadChildren: () => import('../app/modules/user/user.module').then(m => m.UserModule)
       },
+      {
+        path: 'login',
+        loadChildren: () => import('../app/modules/login/login.module').then(m => m.LoginModule)
+      },
     ]
 
 @NgModule({
---
+++ src/app/modules/login/login-routing.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+
+const routes: Routes = [
+  {
+    path: 'login',
+    title: 'Inicio de sesión',
+    component: LoginComponent
+  },
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class LoginRoutingModule { }
