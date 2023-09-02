e70e7d56a4b6d07efb31374a679b7acbe9e414e3
Jose Mario Barrios <jmbarrios@indracompany.com>
2023-09-01 20:52:00 -0500

    cambio de nombre

--- src/app/modules/app/components/app/app.component.spec.ts
+++ src/app/modules/app/components/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'prueba'`, () => {
+  it(`should have as title 'sihsu-frontend'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('prueba');
+    expect(app.title).toEqual('sihsu-frontend');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('prueba app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('sihsu-frontend app is running!');
   });
 });
